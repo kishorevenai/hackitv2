571a301a0c26a6ea9b4207e3c79ace8fe1815dba
kishorevenai <kishorevenai@gmail.com>
2023-06-14 13:55:03 +0530

    sdf

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
         /></a>
         <nav class="Header_Links">
           <a href="./index.php">Home</a>
-          <a href="./services.php">Our Services</a>
+          <a href="./services">Our Services</a>
           <a href="./portfolio.php">Portifolio and Skillset</a>
           <a href="./company.php">Company</a>
           <a href="./contactUs.php">Contact us</a>
